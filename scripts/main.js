fb12c87c01d43e8815e3c81abd4f1d6de2f1c288
Joseph Stocks <jpstocks63@gmail.com>
2020-12-09 16:34:04 -0600

    working points counting code

--- scripts/main.js
+++ scripts/main.js
@@ -22,6 +22,14 @@ const fillNewDeck = () => {
     for (const suit of ["clubs", "diamonds", "hearts", "spades"]) {
         for (let rank = 1; rank <= 13; rank++) {
             name = rankToName(rank);
+
+            if (rank === 1) {
+                value = [1, 11];
+            } else if (rank > 10) {
+                value = [10];
+            } else {
+                value = [rank];
+            }
             deck.push({
                 rank: rank,
                 suit: suit,
@@ -30,6 +38,7 @@ const fillNewDeck = () => {
                     1
                 )} of ${suit[0].toUpperCase()}${suit.slice(1)}`,
                 imageFilePath: `${imageDir}/${name}_of_${suit}.png`,
+                value: value,
             });
         }
     }
@@ -54,7 +63,7 @@ const renderNewCard = (hand, card) => {
 const dealOneCard = (hand) => {
     card = deck.pop();
     hand.cards.push(card);
-    renderNewCard(hand, card);
+    // renderNewCard(hand, card);
 };
 
 const initialDeal = () => {
@@ -66,31 +75,70 @@ const initialDeal = () => {
 };
 
 const clearImagesFromHandDiv = (hand) => {
-    const handDiv = document.getElementById(`${hand.owner}-hand`)
+    const handDiv = document.getElementById(`${hand.owner}-hand`);
     while (handDiv.firstChild) {
-        handDiv.firstChild.remove()
+        handDiv.firstChild.remove();
     }
-}
+};
 
 const renderHand = (hand) => {
-    clearImagesFromHandDiv(hand)
+    clearImagesFromHandDiv(hand);
     for (const card of hand.cards) {
-        renderNewCard(hand, card)
+        renderNewCard(hand, card);
     }
-}
+};
 
 let deck = [];
 fillNewDeck();
 let dealerHand = { owner: "dealer", cards: [] };
 let playerHand = { owner: "player", cards: [] };
 initialDeal();
-// renderHand(dealerHand)
-// renderHand(playerHand)
+renderHand(dealerHand);
+renderHand(playerHand);
 
 console.log(deck);
 console.log(playerHand);
 console.log(dealerHand);
 
-const calculatePoints = (hand) => {
-    
+// const createCombinations() {
+
+// }
+
+function createPointCombinationArrays(array) {
+    var results = [[]];
+    for (var i = 0; i < array.length; i++) {
+        var currentSubArray = array[i];
+        var temp = [];
+        for (var j = 0; j < results.length; j++) {
+            for (var k = 0; k < currentSubArray.length; k++) {
+                temp.push(results[j].concat(currentSubArray[k]));
+            }
+        }
+        results = temp;
+    }
+    return results;
 }
+
+const sumArray = (arr) => {
+    return arr.reduce(
+        (accumulator, currentValue) => accumulator + currentValue
+    );
+};
+
+const calculatePoints = (hand) => {
+    valuesArr = [];
+    for (const card of hand.cards) {
+        valuesArr.push(card.value);
+    }
+    // valueCombosArr = createCombinations(valuesArr)
+    valueCombosArr = createPointCombinationArrays(valuesArr);
+    for (const valueArr of valueCombosArr) {
+        let sum = sumArray(valueArr)
+        console.log(sum);
+    }
+};
+
+calculatePoints(dealerHand);
+calculatePoints(playerHand);
+
+// console.log(sumArray([1, 2, 3]));
